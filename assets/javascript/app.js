0cfbeabcb1efdbf3407a2894489c384684f1f6af
ericmagallan <ericmagallan85@gmail.com>
2018-12-02 23:01:24 -0800

    Game logic entered into app.js.

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+// This is where my global variables will sit
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// 15 Second TImer
+
+//This is where my questions will sit
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// var question[] #0. This array and any future arrays will contain 10 questions each.
+
+//This is where my answers will sit
+// var answers[] #0. This array andy any future arrays will contain 10 answers eaach.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//This is where the timer will sit.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//Page loads
+    //Game Begins
+        //Timer loads: 15secs or 15000;
+        //Question loads
+        //Answer loads w/ radio buttons
+        //Background loads
+
+    //Game starts
+        //GameLoop
+                //Timer starts
+                    //Timer counts down i--;
+                    //User clicks radio button
+                        //If not done on time: 0 point logged;
+                        //If answer = false: 0 point logged;
+                        //If answer= true: 1 point logged;
+                //If answer =true then change color of correct answer to Green && image will pop up of the correct indicated answer
+                //If answer =false then change color of selected answer to red && change correct answer to green && image will pop up of the correct indicated answer
+                
+                    //Timer counts down i--; then continue to next page after display true or false answer, then
+                    //both timers reset &&
+        //GameLoops up to, but not exceeding 10 intervals.
+        //Once player completes 10th loop, display pops up that outputs total correct && total incorrect
+    //Reset button to restart game
+        //Counter will reset, points will reset
+
+    //Future rendition of game will have multiple arrays banks of questions that will be randomly selected
+    //to add variety to the game if the player chooses to reset.
+
+            
+        
